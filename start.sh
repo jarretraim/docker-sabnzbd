f745ec3541e8798224ef03039b7b2b0623caaa0f
Jarret Raim <jarito@gmail.com>
2015-01-17 14:50:24 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-/usr/bin/sabnzbdplus --daemon --config-file /config --server :8080
+/usr/bin/sabnzbdplus --daemon --config-file /mnt/config --server :8080
 sleep 5
 
-tail -f /config/logs/sabnzbd.*
+tail -f /mnt/config/logs/sabnzbd.*
